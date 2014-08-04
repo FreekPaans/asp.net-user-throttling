3a42903771b778ae922ebdf4ad54b369734b8b1d
freek <freek@infi.nl>
2014-08-04 19:41:39 +0200

    initial commit

---
+++ UserThrottling/UserThrottling/UserThrottlingActionFilterAttribute.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+
+namespace UserThrottling {
+	public class UserThrottlingActionFilterAttribute : ActionFilterAttribute{
+	}
+}
